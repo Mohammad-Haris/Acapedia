e049983cb512e26fdd149a231bc6e01faf99b012
Muhammad Haris <Agha.Mohammad.Haris@gmail.com>
2023-03-05 23:38:28 +0500

    Add caching for Explore Db calls, update code generation package, and remove AspNetCore framework reference.

--- Acapedia.Service/ExploreService.cs
+++ Acapedia.Service/ExploreService.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Acapedia.Data;
 using Acapedia.Data.Contracts;
 using Acapedia.Data.ViewModels.ExploreViewModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Newtonsoft.Json.Linq;
 
 namespace Acapedia.Service
@@ -11,64 +13,106 @@ namespace Acapedia.Service
     public class ExploreService : IExplore
     {
         private readonly AcapediaDbContext _Context;
+        private readonly IMemoryCache _memoryCache;
 
-        public ExploreService (AcapediaDbContext context)
+        public ExploreService(AcapediaDbContext context, IMemoryCache memoryCache)
         {
             _Context = context;
+            _memoryCache = memoryCache;
         }
 
-        public IEnumerable<WebsiteLinkModel> GetUniversities (JArray _ClientSelection)
+        public IEnumerable<WebsiteLinkModel> GetUniversities(JArray _ClientSelection)
         {
+            var country = _ClientSelection[0].ToString();
+            var discipline = _ClientSelection[1].ToString();
+            var cacheKey = GetCacheKey(country, discipline);
+
+            if (_memoryCache.TryGetValue(cacheKey, out List<WebsiteLinkModel> cacheValue))
+            {
+                return cacheValue;
+            }
+
             var _Countries = _Context.Country.AsNoTracking().Where(coun => coun.CountryName != "Online").Select(coun => coun.CountryName.ToLower());
             var _Disciplines = _Context.Discipline.AsNoTracking().Select(discip => discip.DisciplineName.ToLower());
 
-            string _Country = _ClientSelection[0].ToString();
-            string _Discipline = _ClientSelection[1].ToString();
-
-            if (_Countries.ToList().Contains(_Country.ToLower()) && _Disciplines.ToList().Contains(_Discipline.ToLower()))
+            if (_Countries.Any(x => x == country.ToLower()) && _Disciplines.Any(x => x == discipline.ToLower()))
             {
-                string _DiscipId = _Context.Discipline.AsNoTracking().Where(dis => dis.DisciplineName == _Discipline).Select(dis => dis.DisciplineId).FirstOrDefault();
-                var _QueryResult = _Context.WebsiteLink.AsNoTracking().Where(sel => sel.LinkCountryName == _Country).Where(sel => sel.LinkDisciplineId == _DiscipId)
-                    .Select(sel => new WebsiteLinkModel
-                    {
-                        LinkUrl = sel.LinkUrl,
-                        Title = sel.Title,
-                        Description = sel.Description
-                    });
+                string discipId =
+                    _Context.Discipline.AsNoTracking().Where(dis => dis.DisciplineName == discipline).Select(dis => dis.DisciplineId).FirstOrDefault();
 
-                return _QueryResult.ToList();
-            }
-
-            else
-            {
-                return new List<WebsiteLinkModel>();
-            }
-        }
-
-        public IEnumerable<WebsiteLinkModel> GetOnline (JArray _ClientSelection)
-        {
-            var _Disciplines = _Context.Discipline.AsNoTracking().Select(discip => discip.DisciplineName.ToLower());
-
-            var _Country = "Online";
-            var _Discipline = _ClientSelection[0].ToString();
-
-            if (_Disciplines.ToList().Contains(_Discipline.ToLower()))
-            {
-                string _DiscipId = _Context.Discipline.AsNoTracking().Where(dis => dis.DisciplineName == _Discipline).Select(dis => dis.DisciplineId).FirstOrDefault();
-                var _QueryResult = _Context.WebsiteLink.AsNoTracking().Where(sel => sel.LinkCountryName == _Country).Where(sel => sel.LinkDisciplineId == _DiscipId)
-                    .Select(sel => new WebsiteLinkModel {
-                        LinkUrl = sel.LinkUrl,
-                        Title = sel.Title,
-                        Description = sel.Description
+                IQueryable<WebsiteLinkModel> queryResult =
+                    _Context.WebsiteLink.AsNoTracking().Where(sel => sel.LinkCountryName == country).Where(sel => sel.LinkDisciplineId == discipId)
+                        .Select(sel => new WebsiteLinkModel
+                        {
+                            LinkUrl = sel.LinkUrl,
+                            Title = sel.Title,
+                            Description = sel.Description
                         });
 
-                return _QueryResult.ToList();
+                MemoryCacheEntryOptions cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromHours(12))
+                    .SetAbsoluteExpiration(TimeSpan.FromHours(24));
+
+                cacheValue = queryResult.ToList();
+
+                _memoryCache.Set(cacheKey, cacheValue, cacheEntryOptions);
             }
 
-            else
+            if (cacheValue != default)
             {
-                return new List<WebsiteLinkModel>();
+                return cacheValue;
             }
+
+            return new List<WebsiteLinkModel>();
+        }
+
+        public IEnumerable<WebsiteLinkModel> GetOnline(JArray _ClientSelection)
+        {
+            var country = "Online";
+            var discipline = _ClientSelection[0].ToString();
+            var cacheKey = GetCacheKey(country, discipline);
+
+            if (_memoryCache.TryGetValue(cacheKey, out List<WebsiteLinkModel> cacheValue))
+            {
+                return cacheValue;
+            }
+
+            var _Disciplines = _Context.Discipline.AsNoTracking().Select(discip => discip.DisciplineName.ToLower());
+
+            if (_Disciplines.Any(x => x == discipline.ToLower()))
+            {
+                string _DiscipId =
+                    _Context.Discipline.AsNoTracking().Where(dis => dis.DisciplineName == discipline).Select(dis => dis.DisciplineId).FirstOrDefault();
+
+                IQueryable<WebsiteLinkModel> _QueryResult =
+                    _Context.WebsiteLink.AsNoTracking().Where(sel => sel.LinkCountryName == country).Where(sel => sel.LinkDisciplineId == _DiscipId)
+                        .Select(sel => new WebsiteLinkModel
+                        {
+                            LinkUrl = sel.LinkUrl,
+                            Title = sel.Title,
+                            Description = sel.Description
+                        });
+
+                MemoryCacheEntryOptions cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromHours(12))
+                    .SetAbsoluteExpiration(TimeSpan.FromHours(24));
+
+                cacheValue = _QueryResult.ToList();
+
+                _memoryCache.Set(cacheKey, cacheValue, cacheEntryOptions);
+            }
+
+            if (cacheValue != default)
+            {
+                return cacheValue;
+            }
+
+            return new List<WebsiteLinkModel>();
+        }
+
+        private string GetCacheKey(string country, string discipline)
+        {
+            return $"{country}--{discipline}";
         }
     }
 }
